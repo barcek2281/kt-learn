30f6c61774704e401e8bca8967584d2de27a3786
barcek2281 <sabdpp17@gmail.com>
2026-07-15 23:38:59 +0500

    Add project files.

---
+++ Controllers/AuthRestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KT_Learn.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthRestController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Hello()
+        {
+            return Ok(new { message = "Hello" });
+        }
+    }
+}
